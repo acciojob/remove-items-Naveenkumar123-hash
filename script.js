eabfe1898cb8b0a660532512e243779133d9e7c9
Naveenkumar123-hash <120244421+Naveenkumar123-hash@users.noreply.github.com>
2023-06-19 17:39:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function removeColor(color) {
+  var colorList = document.getElementById("colorSelect");
+  var colorItems = colorList.getElementsByTagName("option");
+
+  for (var i = 0; i < colorItems.length; i++) {
+    var item = colorItems[i];
+    if (item.firstChild.nodeValue.trim() === color) {
+      colorList.removeChild(item);
+      break;
+    }
+  }
+}
+
+
+
+
